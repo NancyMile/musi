1cfa5323963663e4fa0b0f02eb2cec50c350aaf5
nancy <smithnancy@outlook.com>
2023-07-22 15:16:26 +1000

    generating webp images with gulp-webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-const { src, dest, watch} = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+//css
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+//img
+const webp = require('gulp-webp');
 
 function css(done) {
     src('src/scss/**/*.scss') //find sass file
@@ -16,5 +19,18 @@ function dev(done) {
     done();
 }
 
+function versionWebp( done ){
+    const options = {
+        quality: 50
+    }
+
+    src('src/img/**/*.{jpg,png}')
+        .pipe(webp(options))
+        .pipe(dest("build/img"))
+
+    done();
+}
+
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev = parallel( versionWebp, dev );
