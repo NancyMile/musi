28565225fd101581b5d5e86146642caf59e84bc8
nancy <smithnancy@outlook.com>
2023-07-23 20:48:44 +1000

    app js html for the gallery created

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,22 @@
-alert("test");
+document.addEventListener('DOMContentLoaded', function() {
+    initApp();
+});
+
+function initApp() {
+    createGallery();
+}
+
+function createGallery() {
+    const gallery = document.querySelector('.images-gallery');
+    for (let i = 1; i <= 12; i++) {
+
+        const image = document.createElement('picture');
+        image.innerHTML = `
+            <source srcset="build/img/thumb/${i}.avif" type="image/avif">
+            <source srcset="build/img/thumb/${i}.webp" type="image/webp">
+            <img  loading="lazy" width="200" height ="300" src="build/img/thumb/${i}.jpg" alt="image gallery" />
+        `;
+
+        gallery.appendChild(image);
+    }
+}
